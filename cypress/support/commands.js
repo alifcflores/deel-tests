13c6593860aa3da5a26611e10cd34a7919da5d5a
Alif Corrêa Flores <alif.flores@credpago.com>
2021-12-27 23:01:14 -0300

    setting configs of the project

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,16 +1,18 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+const params = require('../fixtures/parameters.json');
+const API = Cypress.env('API');
+
+Cypress.Commands.add('login', () => {
+    cy.intercept('GET', API + '/commons/countries/biMonthly').as('commons/countries/biMonthly');
+    cy.visit('/login');
+    cy.wait('@commons/countries/biMonthly');
+    cy.get('[name="email"]').type(params.user);
+    cy.get('[name="password"]').type(params.pass);
+
+    cy.intercept('POST', API + '/login').as('login');
+    cy.get('form').submit();
+    cy.wait('@login');
+});
+
 //
 //
 // -- This is a child command --
