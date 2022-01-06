fffa303d87f3dadaf0dac5b8e90ec9d7a1cb3e6b
alifcflores <90803275+alifcflores@users.noreply.github.com>
2022-01-05 21:37:38 -0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,7 +1,9 @@
 function countNumbers(stringWithNumbers){
     const positions = new Array(10);
     for(i = 0; i < positions.length; i++){
+        //create a regular expression with the number of iterator, the second parameters gi is to search for all the results (g is to uppercase and i is to lowercase)
         let regularExpression = new RegExp(i, 'gi');
+        //using the regular expression to match a number in a string passed like an argument on function.
         positions[i] = stringWithNumbers.match(regularExpression).length;
     }
     return positions;
